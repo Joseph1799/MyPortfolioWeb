c49e0c358c6329e9ca5ab4a33a4ceebecf65db8e
Joseph1799 <Joseph1799us@gmail.com>
2024-06-18 02:57:04 -0600

    Update gitignore and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
+}
 
 module.exports = nextConfig
